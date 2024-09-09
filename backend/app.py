4ab8e2d4a39bfcf401e8563fb55bf956810ae580
meranamkarthikeyavarma <seetarama07@gmail.com>
2024-09-09 14:39:28 +0530

    chnagingflaskport

--- backend/app.py
+++ backend/app.py
@@ -66,4 +66,5 @@ except Exception as e:
     # host='0.0.0.0', port=int(os.environ.get('PORT', 10000))
 
 if __name__ == "__main__":
-    app.run(debug =True, port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Get the port from environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
